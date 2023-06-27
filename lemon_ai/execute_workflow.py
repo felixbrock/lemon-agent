b3bf16f43b9b6060572aa1c9186a4eb4547eb03c
aah120 <aah120@ic.ac.uk>
2023-06-27 15:49:12 +0100

    implement filtering tools list to make more scalable

--- lemon_ai/execute_workflow.py
+++ lemon_ai/execute_workflow.py
@@ -5,6 +5,7 @@ from langchain.agents import initialize_agent, AgentType
 from lemon_ai.get_integrations import get_apis_from_env
 from lemon_ai.cito_api_wrapper import CitoAPIWrapper
 from lemon_ai.cito_toolkit import CitoToolkit
+from lemon_ai.filter_tools import filter_tools
 
 def execute_workflow(llm: BaseLLM, prompt_string: str):
 
@@ -19,17 +20,17 @@ def execute_workflow(llm: BaseLLM, prompt_string: str):
     toolkit = CitoToolkit.from_cito_api_wrapper(cito_wrapper, api_keys_dict, access_tokens_dict, logger, str(session_id))
     tools = toolkit.get_tools()
 
+    prompt = f"Your task is '{prompt_string}'. Focus on the ordering of the tasks given. Do not use a workflow unless it is mentioned. Give your action input as a valid JSON object where the keys are the params and the values are the value for each input parameter. If a param is optional and you have not been given a value, do not include that field in the input. Your final answer should give a brief conversational overview of what you did."
+    
+    filtered_tools = filter_tools(llm=llm, task=prompt_string, tools=tools)
+    
     agent = initialize_agent(
-        tools=tools,
+        tools=filtered_tools,
         llm=llm,
-        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, 
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
         verbose=True
     )
 
-    prompt = prompt_string + "Give your action input as a valid JSON object where the keys are the params \
-        and the values are the value for each input parameter. If a param is optional and you have not been given a value, \
-        do not include that field in the input. Your final answer should give a brief conversational overview of what you did \
-        and a description of the content of the result's values."
     answer = agent.run(prompt)
 
     logger.remove()
